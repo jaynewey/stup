59158b1d95407d11154f487ad05be1996b7ab4e4
Jay Newey <jayluxray@gmail.com>
2020-05-24 13:47:41 +0100

    Annotate family.py

--- family.py
+++ family.py
@@ -1,8 +1,15 @@
 class Family:
+    """Data structure for tracking sets of entities that have specific Component types."""
+
     def __init__(self, entities):
         self._entities = entities
 
     def set_entities(self, entities):
+        """
+
+        :param entities: A set of Entity instances.
+        :return: None
+        """
         self._entities = entities
 
     def __iter__(self):
