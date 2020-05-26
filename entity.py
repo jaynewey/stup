628a277764ecff0029926eb40fa4777b8cdfa62f
Jay Newey <jayluxray@gmail.com>
2020-05-26 13:14:10 +0100

    Remove uuid from entity.py

--- entity.py
+++ entity.py
@@ -1,7 +1,4 @@
-import uuid
-
-
 class Entity:
-    """Simple holders of Universally Unique Identifiers (UUIDs) used to identify this Entity in the Entity Manager."""
+    """Simple Universally Unique Identifiers (UUIDs) used to identify this Entity in the Entity Manager."""
     def __init__(self):
-        self.uuid = uuid.uuid4()
+        pass
