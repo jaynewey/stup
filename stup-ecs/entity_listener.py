9333a3b1ce1ce2e2527acf5b11c8ed5ba43fa6cd
Jay Newey <jayluxray@gmail.com>
2020-07-05 09:41:20 +0100

    Update docstring

--- stup-ecs/entity_listener.py
+++ stup-ecs/entity_listener.py
@@ -7,16 +7,19 @@ class EntityListener(ABC):
 
     @abstractmethod
     def entity_added(self, entity):
-        """This method gets called when an entity is added.
+        """This method gets called when an entity is added to a manager this listener is registered to.
 
         :param entity: The entity that was added
         """
         pass
 
     @abstractmethod
-    def entity_removed(self, entity):
-        """This method gets called when an entity is removed.
+    def entity_removed(self, entity, components):
+        """This method gets called when an entity is removed from a manager this listener is registered to.
 
         :param entity: The entity that was removed
+        :type entity: Entity
+        :param components: The components that were attached to the entity in the system
+        :type components: set
         """
         pass
